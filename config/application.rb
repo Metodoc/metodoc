5f81fca0552b4add62bd20c2347d16b9edcf9d05
Jean Carlos Novaes <jeancarlosnovaes@yahoo.com.br>
2017-01-18 19:34:54 -0300

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module Metodoc
         # config/application.rb
         config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
-
+        config.assets.initialize_on_precompile = false
+        
         # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
         # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
         config.time_zone = 'Brasilia'
