c92360541c09962c3537f2e24af4aa4a7369ff62
Jean Carlos Novaes <jeancarlosnovaes@yahoo.com.br>
2017-01-19 02:26:59 -0300

    assets compiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
     # config.assets.css_compressor = :sass
 
     # Do not fallback to assets pipeline if a precompiled asset is missed.
-    config.assets.compile = false
+    config.assets.compile = true
 
     # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -62,7 +62,7 @@ Rails.application.configure do
 
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
-    host = '<your heroku app>.herokuapp.com'
+    host = 'metodoc.herokuapp.com'
     config.action_mailer.default_url_options = { host: host }
     ActionMailer::Base.smtp_settings = {
         :address        => 'smtp.sendgrid.net',
